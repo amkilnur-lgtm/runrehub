cef499264a533983ef71db35d73209c9c26726e1
RunRehab Dev <dev@runrehab.local>
2026-03-17 14:59:01 +0300

    Fix logout request headers

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,10 +1,12 @@
 export async function api<T>(url: string, options?: RequestInit): Promise<T> {
+  const headers = new Headers(options?.headers);
+  if (options?.body && !headers.has("Content-Type")) {
+    headers.set("Content-Type", "application/json");
+  }
+
   const response = await fetch(url, {
     credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-      ...(options?.headers ?? {})
-    },
+    headers,
     ...options
   });
 
