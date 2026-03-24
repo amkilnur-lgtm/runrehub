ffeffef3381f374e5a6f2e3e3166dad4c0bb8f69
RunRehab Dev <dev@runrehab.local>
2026-03-24 15:36:07 +0500

    Refine workout map palette

--- client/src/components/WorkoutRouteMap.tsx
+++ client/src/components/WorkoutRouteMap.tsx
@@ -56,7 +56,7 @@ export function WorkoutRouteMap({ points }: { points: [number, number][] }) {
         />
         <TileLayer
           url="https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}{r}.png"
-          opacity={0.72}
+          opacity={0.68}
           attribution=""
         />
         <FitRouteBounds points={points} />
